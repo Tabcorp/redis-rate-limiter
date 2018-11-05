a337526e149fc933c759dae9013a9975e6427e05
Hasan Özdemir <hasan.ozdemir@nodify.at>
2018-11-05 11:55:43 +0100

    - improve tests to prevent duplicates while testing multiple clients

--- test/middleware.spec.js
+++ test/middleware.spec.js
@@ -1,117 +1,119 @@
-var _            = require('lodash');
-var async        = require('async');
-var should       = require('should');
-var redis        = require('redis');
-var express      = require('express');
-var supertest    = require('supertest');
-var reset        = require('./reset');
-var middleware   = require('../lib/middleware');
+const _            = require('lodash');
+const async        = require('async');
+const express      = require('express');
+const supertest    = require('supertest');
+const reset        = require('./reset');
+const middleware   = require('../lib/middleware');
 
-describe('Middleware', function() {
+['redis', 'ioredis'].forEach(clientName => {
+  const redis        = require(clientName);
 
-  this.slow(5000);
-  this.timeout(5000);
+  describe(`${clientName}: Middleware`, function() {
 
-  var client  = null;
-  var limiter = null;
+    this.slow(5000);
+    this.timeout(5000);
 
-  before(function(done) {
-    client = redis.createClient(6379, 'localhost', {enable_offline_queue: false});
-    client.on('ready', done);
+    let client  = null;
+    let limiter = null;
+
+    before(function(done) {
+      client = redis.createClient(8002, 'localhost', {enable_offline_queue: false});
+      client.on('ready', done);
+    });
+
+    beforeEach(function(done) {
+      reset.allkeys(client, done);
+    });
+
+    after(function() {
+      client.quit();
+    });
+
+    describe('IP throttling', function() {
+
+      before(function() {
+        limiter = middleware({
+          redis: client,
+          key: 'ip',
+          rate: '10/second'
+        });
+      });
+
+      it('passes through under the limit', function(done) {
+        const server = express();
+        server.use(limiter);
+        server.use(okResponse);
+        const reqs = requests(server, 9, '/test');
+        async.parallel(reqs, function(err, data) {
+          withStatus(data, 200).should.have.length(9);
+          done();
+        });
+      });
+
+      it('returns HTTP 429 over the limit', function(done) {
+        const server = express();
+        server.use(limiter);
+        server.use(okResponse);
+        const reqs = requests(server, 12, '/test');
+        async.parallel(reqs, function(err, data) {
+          withStatus(data, 200).should.have.length(10);
+          withStatus(data, 429).should.have.length(2);
+          done();
+        });
+      });
+
+      it('works across several rate-limit windows', function(done) {
+        const server = express();
+        server.use(limiter);
+        server.use(okResponse);
+        async.series([
+          parallelRequests(server, 9, '/test'),
+          wait(1100),
+          parallelRequests(server, 12, '/test'),
+          wait(1100),
+          parallelRequests(server, 9, '/test')
+        ], function(err, data) {
+          withStatus(data[0], 200).should.have.length(9);
+          withStatus(data[2], 200).should.have.length(10);
+          withStatus(data[2], 429).should.have.length(2);
+          withStatus(data[4], 200).should.have.length(9);
+          done();
+        });
+      });
+
+    });
+
+    describe('Custom key throttling', function() {
+
+      before(function() {
+        limiter = middleware({
+          redis: client,
+          key: function(req) { return req.query.user; },
+          rate: '10/second'
+        });
+      });
+
+      it('uses a different bucket for each custom key (user)', function(done) {
+        const server = express();
+        server.use(limiter);
+        server.use(okResponse);
+        const reqs = _.flatten([
+          requests(server,  5, '/test?user=a'),
+          requests(server, 12, '/test?user=b'),
+          requests(server, 10, '/test?user=c')
+        ]);
+        async.parallel(reqs, function(err, data) {
+          withStatus(data, 200).should.have.length(25);
+          withStatus(data, 429).should.have.length(2);
+          withStatus(data, 429)[0].url.should.eql('/test?user=b');
+          withStatus(data, 429)[1].url.should.eql('/test?user=b');
+          done();
+        });
+      });
+
+    });
+
   });
-
-  beforeEach(function(done) {
-    reset.allkeys(client, done);
-  });
-
-  after(function() {
-    client.quit();
-  });
-
-  describe('IP throttling', function() {
-
-    before(function() {
-      limiter = middleware({
-        redis: client,
-        key: 'ip',
-        rate: '10/second'
-      });
-    });
-
-    it('passes through under the limit', function(done) {
-      var server = express();
-      server.use(limiter);
-      server.use(okResponse);
-      var reqs = requests(server, 9, '/test');
-      async.parallel(reqs, function(err, data) {
-        withStatus(data, 200).should.have.length(9);
-        done();
-      });
-    });
-
-    it('returns HTTP 429 over the limit', function(done) {
-      var server = express();
-      server.use(limiter);
-      server.use(okResponse);
-      var reqs = requests(server, 12, '/test');
-      async.parallel(reqs, function(err, data) {
-        withStatus(data, 200).should.have.length(10);
-        withStatus(data, 429).should.have.length(2);
-        done();
-      });
-    });
-
-    it('works across several rate-limit windows', function(done) {
-      var server = express();
-      server.use(limiter);
-      server.use(okResponse);
-      async.series([
-        parallelRequests(server, 9, '/test'),
-        wait(1100),
-        parallelRequests(server, 12, '/test'),
-        wait(1100),
-        parallelRequests(server, 9, '/test')
-      ], function(err, data) {
-        withStatus(data[0], 200).should.have.length(9);
-        withStatus(data[2], 200).should.have.length(10);
-        withStatus(data[2], 429).should.have.length(2);
-        withStatus(data[4], 200).should.have.length(9);
-        done();
-      });
-    });
-
-  });
-
-  describe('Custom key throttling', function() {
-
-    before(function() {
-      limiter = middleware({
-        redis: client,
-        key: function(req) { return req.query.user; },
-        rate: '10/second'
-      });
-    });
-
-    it('uses a different bucket for each custom key (user)', function(done) {
-      var server = express();
-      server.use(limiter);
-      server.use(okResponse);
-      var reqs = _.flatten([
-        requests(server,  5, '/test?user=a'),
-        requests(server, 12, '/test?user=b'),
-        requests(server, 10, '/test?user=c')
-      ]);
-      async.parallel(reqs, function(err, data) {
-        withStatus(data, 200).should.have.length(25);
-        withStatus(data, 429).should.have.length(2);
-        withStatus(data, 429)[0].url.should.eql('/test?user=b');
-        withStatus(data, 429)[1].url.should.eql('/test?user=b');
-        done();
-      });
-    });
-
-  });
-
 });
 
 function requests(server, count, url) {
@@ -140,7 +142,7 @@ function okResponse(req, res, next) {
 }
 
 function withStatus(data, code) {
-  var pretty = data.map(function(d) {
+  const pretty = data.map(function(d) {
     return {
       url: d.req.path,
       statusCode: d.res.statusCode,
--- test/rate-limiter.spec.js
+++ test/rate-limiter.spec.js
@@ -1,122 +1,124 @@
-var _            = require('lodash');
-var async        = require('async');
-var should       = require('should');
-var redis        = require('redis');
-var reset        = require('./reset');
-var rateLimiter  = require('../lib/rate-limiter');
+const _            = require('lodash');
+const async        = require('async');
+const reset        = require('./reset');
+const rateLimiter  = require('../lib/rate-limiter');
 
-describe('Rate-limiter', function() {
+[ 'redis', 'ioredis' ].forEach(clientName => {
+  const redis = require(clientName);
 
-  this.slow(5000);
-  this.timeout(5000);
+  describe(`${clientName}: Rate-limiter`, function() {
+    this.slow(5000);
+    this.timeout(5000);
 
-  var client = null;
+    let client = null;
 
-  before(function(done) {
-    client = redis.createClient(6379, 'localhost', {enable_offline_queue: false});
-    client.on('ready', done);
-  });
+    before(function(done) {
+      client = redis.createClient(8002, 'localhost', {enable_offline_queue: false});
+      client.on('ready', done);
+    });
 
-  beforeEach(function(done) {
-    reset.allkeys(client, done);
-  });
+    beforeEach(function(done) {
+      reset.allkeys(client, done);
+    });
 
-  after(function() {
-    client.quit();
-  });
+    after(function() {
+      client.quit();
+    });
 
-  it('calls back with the rate data', function(done) {
-    var limiter = createLimiter('10/second');
-    var reqs = request(limiter, 5, {id: 'a'});
-    async.parallel(reqs, function(err, rates) {
-      _.map(rates, 'current').should.eql([1, 2, 3, 4, 5]);
-      _.each(rates, function(r) {
-        r.key.should.eql('a');
-        r.limit.should.eql(10);
-        r.window.should.eql(1);
-        r.over.should.eql(false);
+    it('calls back with the rate data', function(done) {
+      const limiter = createLimiter('10/second');
+      const reqs = request(limiter, 5, {id: 'a'});
+      async.parallel(reqs, function(err, rates) {
+        _.map(rates, 'current').should.eql([1, 2, 3, 4, 5]);
+        _.each(rates, function(r) {
+          r.key.should.eql('a');
+          r.limit.should.eql(10);
+          r.window.should.eql(1);
+          r.over.should.eql(false);
+        });
+        done();
       });
-      done();
     });
-  });
 
-  it('sets the over flag when above the limit', function(done) {
-    var limiter = createLimiter('10/second');
-    var reqs = request(limiter, 15, {id: 'a'});
-    async.parallel(reqs, function(err, rates) {
-      _.each(rates, function(r, index) {
-        rates[index].over.should.eql(index >= 10);
+    it('sets the over flag when above the limit', function(done) {
+      const limiter = createLimiter('10/second');
+      const reqs = request(limiter, 15, {id: 'a'});
+      async.parallel(reqs, function(err, rates) {
+        _.each(rates, function(r, index) {
+          rates[index].over.should.eql(index >= 10);
+        });
+        done();
       });
-      done();
     });
-  });
 
-  it('uses one bucket per key', function(done) {
-    var limiter = createLimiter('10/second');
-    var reqs = _.flatten([
-      request(limiter, 10, {id: 'a'}),
-      request(limiter, 12, {id: 'b'}),
-      request(limiter, 10, {id: 'c'})
-    ]);
-    async.parallel(reqs, function(err, rates) {
-      _.filter(rates, {over: true}).should.have.length(2);
-      done();
-    });
-  });
-
-  it('can handle a lot of requests', function(done) {
-    var limiter = createLimiter('1000/second');
-    var reqs = request(limiter, 1200, {id: 'a'});
-    async.parallel(reqs, function(err, rates) {
-      rates[999].should.have.property('over', false);
-      rates[1000].should.have.property('over', true);
-      done();
-    });
-  });
-
-  it('resets after the window', function(done) {
-    var limiter = createLimiter('10/second');
-    async.series([
-      requestParallel(limiter, 15, {id: 'a'}),
-      wait(1100),
-      requestParallel(limiter, 15, {id: 'a'})
-    ], function(err, data) {
-      _.each(data[0], function(rate, index) {
-        rate.should.have.property('over', index > 9);
+    it('uses one bucket per key', function(done) {
+      const limiter = createLimiter('10/second');
+      const reqs = _.flatten([
+        request(limiter, 10, {id: 'a'}),
+        request(limiter, 12, {id: 'b'}),
+        request(limiter, 10, {id: 'c'})
+      ]);
+      async.parallel(reqs, function(err, rates) {
+        _.filter(rates, {over: true}).should.have.length(2);
+        done();
       });
-      _.each(data[2], function(rate, index) {
-        rate.should.have.property('over', index > 9);
+    });
+
+    it('can handle a lot of requests', function(done) {
+      const limiter = createLimiter('1000/second');
+      const reqs = request(limiter, 1200, {id: 'a'});
+      async.parallel(reqs, function(err, rates) {
+        rates[999].should.have.property('over', false);
+        rates[1000].should.have.property('over', true);
+        done();
       });
-      done();
     });
-  });
 
-  function createLimiter(rate) {
-    return rateLimiter({
-      redis: client,
-      key: function(x) { return x.id },
-      rate: rate
+    it('resets after the window', function(done) {
+      const limiter = createLimiter('10/second');
+      async.series([
+        requestParallel(limiter, 15, {id: 'a'}),
+        wait(1100),
+        requestParallel(limiter, 15, {id: 'a'})
+      ], function(err, data) {
+        _.each(data[0], function(rate, index) {
+          rate.should.have.property('over', index > 9);
+        });
+        _.each(data[2], function(rate, index) {
+          rate.should.have.property('over', index > 9);
+        });
+        done();
+      });
     });
-  }
 
-  function request(limiter, count, data) {
-    return _.times(count, function() {
+    function createLimiter(rate) {
+      return rateLimiter({
+        redis: client,
+        key: function(x) { return x.id },
+        rate: rate
+      });
+    }
+
+    function request(limiter, count, data) {
+      return _.times(count, function() {
+        return function(next) {
+          limiter(data, next);
+        };
+      });
+    }
+
+    function requestParallel(limiter, count, data) {
       return function(next) {
-        limiter(data, next);
+        async.parallel(request(limiter, count, data), next);
       };
-    });
-  }
+    }
 
-  function requestParallel(limiter, count, data) {
-    return function(next) {
-      async.parallel(request(limiter, count, data), next);
-    };
-  }
+    function wait(millis) {
+      return function(next) {
+        setTimeout(next, 1100);
+      };
+    }
 
-  function wait(millis) {
-    return function(next) {
-      setTimeout(next, 1100);
-    };
-  }
-
+  });
 });
+
